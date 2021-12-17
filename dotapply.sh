af3a8684a90941ea8dd623ab1d17a2d3ddcecd77
VinceCabs <vctcbns@gmail.com>
2021-12-17 15:36:45 +0100

    better apply script

--- dotapply.sh
+++ dotapply.sh
@@ -7,8 +7,11 @@ hostname: $(hostname)
 OS: $OS
 ============================="
 
-# Dotfiles path
 export DOTFILES_PATH="$HOME/.dotfiles"
+
+link_dotfile() {
+    rm -f ~/$1 && ln -s -f $DOTFILES_PATH/$1 ~/$1
+}
 
 echo "git pull..."
 git -C $DOTFILES_PATH pull
@@ -17,14 +20,11 @@ echo "loading secrets..."
 source $DOTFILES_PATH/.secrets
 
 echo "shell..."
-# source $DOTFILES_PATH/.bash_aliases
-export DOTFILE=".bashrc"
-rm -f ~/$DOTFILE && ln -s -f $DOTFILES_PATH/$DOTFILE ~/$DOTFILE
+link_dotfile .bashrc
 
 # Git
 echo "git..."
-export DOTFILE=".gitconfig"
-rm -f ~/$DOTFILE && ln -s -f $DOTFILES_PATH/$DOTFILE ~/$DOTFILE
+link_dotfile .gitconfig
 git config --global user.email "$GH_EMAIL"  # secret email
 # git config --global credentials.helper wincred  # Windows Only
 
