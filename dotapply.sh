591c33a2d100648687b4e965819094342df0d111
VinceCabs <vctcbns@gmail.com>
2022-09-09 16:57:41 +0200

    add autohotkey scripts

--- dotapply.sh
+++ dotapply.sh
@@ -13,6 +13,12 @@ link_dotfile() {
     rm -rf ~/$1 && ln -s -f $DOTFILES_PATH/$1 ~/$1
 }
 
+setup_autohotkey() {
+    cp $DOTFILES_PATH/bin/AutoHotkey.lnk "$APPDATA/Microsoft/Windows/Start Menu/Programs/Startup";
+
+    echo "...AutoHotkey set on startup (restart required)";
+}
+
 echo "Git pull..."
 git -C $DOTFILES_PATH pull
 
@@ -29,6 +35,12 @@ link_dotfile .gitconfig
 # bins
 echo "Bins..."
 link_dotfile bin
+if [ $OS = "Windows_NT" ]
+then
+    echo "...Windows detected"
+   setup_autohotkey;
+fi
 
 # clean
 unset -f link_dotfile
+unset -f setup_autohotkey
