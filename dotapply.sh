4d7e147e257167103884d63226bb1f763cd6b517
VinceCabs <vctcbns@gmail.com>
2023-10-23 16:05:59 +0200

    format and credits to https://github.com/adriancooney/Taskfile

--- dotapply.sh
+++ dotapply.sh
@@ -6,6 +6,32 @@ set -o nounset
 set -o pipefail
 # set -o xtrace
 
+DOTFILES_PATH="$HOME/.dotfiles"
+
+# UTILS
+
+_link_dotfile() {
+    rm -rf ~/$1 && ln -s -f $DOTFILES_PATH/$1 ~/$1
+}
+
+_setup_autohotkey() {
+    cmd "/c start %USERPROFILE%\.dotfiles\bin\AutoHotkeyU64.exe %USERPROFILE%\.dotfiles\bin\ahk_scripts.ahk"
+    cp $DOTFILES_PATH/bin/AutoHotkey.lnk "$APPDATA/Microsoft/Windows/Start Menu/Programs/Startup";
+    echo "  AutoHotkey started and set on startup";
+}
+
+_install_gh_cli() {
+    type -p curl >/dev/null || sudo apt install curl -y
+    curl -fsSL https://cli.github.com/packages/githubcli-archive-keyring.gpg | sudo dd of=/usr/share/keyrings/githubcli-archive-keyring.gpg \
+    && sudo chmod go+r /usr/share/keyrings/githubcli-archive-keyring.gpg \
+    && echo "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/githubcli-archive-keyring.gpg] https://cli.github.com/packages stable main" | sudo tee /etc/apt/sources.list.d/github-cli.list > /dev/null \
+    && sudo apt update \
+    && sudo apt install gh -y
+}
+
+# MAIN TASKS
+# inspired from (great): https://github.com/adriancooney/Taskfile
+
 show_os_info() {
     echo \
     "=============================
@@ -13,29 +39,6 @@ unamme: $(uname)
 hostname: $(hostname)
 OS: $OS
 ============================="
-}
-
-DOTFILES_PATH="$HOME/.dotfiles"
-
-# UTILS
-
-link_dotfile() {
-    rm -rf ~/$1 && ln -s -f $DOTFILES_PATH/$1 ~/$1
-}
-
-setup_autohotkey() {
-    cmd "/c start %USERPROFILE%\.dotfiles\bin\AutoHotkeyU64.exe %USERPROFILE%\.dotfiles\bin\ahk_scripts.ahk"
-    cp $DOTFILES_PATH/bin/AutoHotkey.lnk "$APPDATA/Microsoft/Windows/Start Menu/Programs/Startup";
-    echo "  AutoHotkey started and set on startup";
-}
-
-install_gh_cli() {
-    type -p curl >/dev/null || sudo apt install curl -y
-    curl -fsSL https://cli.github.com/packages/githubcli-archive-keyring.gpg | sudo dd of=/usr/share/keyrings/githubcli-archive-keyring.gpg \
-    && sudo chmod go+r /usr/share/keyrings/githubcli-archive-keyring.gpg \
-    && echo "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/githubcli-archive-keyring.gpg] https://cli.github.com/packages stable main" | sudo tee /etc/apt/sources.list.d/github-cli.list > /dev/null \
-    && sudo apt update \
-    && sudo apt install gh -y
 }
 
 detect_os() {
@@ -60,9 +63,9 @@ load_secrets() {
 
 link_dotfiles() {
     echo "Link dotfiles..."
-    link_dotfile .bashrc
-    link_dotfile .gitconfig
-    link_dotfile bin
+    _link_dotfile .bashrc
+    _link_dotfile .gitconfig
+    _link_dotfile bin
 }
 
 setup_windows_git() {
@@ -74,17 +77,16 @@ setup_windows_git() {
     fi
 }
 
-
 install_bins() {
     #TODO: update bins (https://gist.github.com/lukechilds/a83e1d7127b78fef38c2914c4ececc3c)
     echo "Bins..."
     if [ $win ]
     then
-        setup_autohotkey;
+        _setup_autohotkey;
     fi
     if [ $linux ] && ! command -v gh &> /dev/null
     then
-        install_gh_cli;
+        _install_gh_cli;
         echo "  Github CLI for Linux installed"
     fi
 }
@@ -99,6 +101,7 @@ default() {
     dotapply
 }
 
+# MAIN
 dotapply() {
     show_os_info
     detect_os
