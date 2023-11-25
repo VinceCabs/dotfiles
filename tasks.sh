9ffafb35af9143b8bc45da315a87511fa917b6d8
VinceCabs <vctcbns@gmail.com>
2023-11-25 17:20:44 +0100

    add new task `rclone_backup`

--- tasks.sh
+++ tasks.sh
@@ -20,6 +20,16 @@ rclone_setup() {
         bucket_policy_only=true
 }
 
+rclone_backup() {
+    # accepts flags as arguments (ex: `--dry-run -vv`)
+    # need a `rclone_filter_list.txt` file located in source, remove if you want to backup all
+    load_secrets
+    rclone sync --progress $@ \
+        --filter-from $RCLONE_BACKUP_SOURCE_PATH/rclone_filter_list.txt \
+        $RCLONE_BACKUP_SOURCE_PATH \
+        $RCLONE_REMOTE_NAME:$RCLONE_GOOGLE_STORAGE_BUCKET
+}
+
 # UTILS
 
 _link_dotfile() {
